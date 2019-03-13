1a7bfec4aba7faeab8be6fe925a296cc87d328fc
btandersen383 <btandersen@utexas.edu>
2019-03-13 13:53:16 -0500

    unhardcoded version for 2 headsets

--- src/openhmd_display.cpp
+++ src/openhmd_display.cpp
@@ -75,11 +75,15 @@ branch on num hmds
     // TODO: this needs to be first in initialize
     // Setup OpenHMD object
     openhmd = new OpenHMD();
-    // TODO: check the return value and respond accordingly
     // return -1, error
     // return 1, 1 hmd
     // return 2, 2 hmds
     NumHMDs = openhmd->init();
+    if (NumHMDs == -1)
+    {
+        std::cout << "Error initializing OpenHMD" << std::endl;
+        exit(1);
+    }
 
     // Set up general objects
     mDispCtx = context_;  // rviz object
@@ -122,7 +126,7 @@ branch on num hmds
     // Window size is set here and may need to be adjusted
     root = rviz::RenderSystem::get()->root();
     mWindow = root->createRenderWindow("HMD1", 2160, 1200, false);
-    mWindow2 = root->createRenderWindow("HMD2", 2160, 1200, false);
+    if (NumHMDs == 2) mWindow2 = root->createRenderWindow("HMD2", 2160, 1200, false);
     std::cout << "Render window created" << std::endl;
 
     // Create the camera node
@@ -131,68 +135,92 @@ branch on num hmds
     else
         mCamera = mSceneMgr->getRootSceneNode()->createChildSceneNode("StereoCameraNode");
 
-    if (mSceneNode)
-        mCamera2 = mSceneNode->createChildSceneNode("StereoCameraNode2");
-    else
-        mCamera2 = mSceneMgr->getRootSceneNode()->createChildSceneNode("StereoCameraNode2");
+    if (NumHMDs == 2)
+    {
+        if (mSceneNode)
+            mCamera2 = mSceneNode->createChildSceneNode("StereoCameraNode2");
+        else
+            mCamera2 = mSceneMgr->getRootSceneNode()->createChildSceneNode("StereoCameraNode2");
+    }
 
     // Create the cameras for hmd1, will act as the eyes
     stereo_cam_left = mSceneMgr->createCamera("StereoCameraLeft");
     stereo_cam_right = mSceneMgr->createCamera("StereoCameraRight");
-    // Create the cameras for hmd2, will act as the eyes
-    stereo_cam_left2 = mSceneMgr->createCamera("StereoCameraLeft2");
-    stereo_cam_right2 = mSceneMgr->createCamera("StereoCameraRight2");
+    if (NumHMDs == 2)
+    {
+        // Create the cameras for hmd2, will act as the eyes
+        stereo_cam_left2 = mSceneMgr->createCamera("StereoCameraLeft2");
+        stereo_cam_right2 = mSceneMgr->createCamera("StereoCameraRight2");
+    }
 
     // Spawn position of the cameras
     // This can be used to manipulate the initial point of view
     // For hmd1
     mCamera->setPosition(Ogre::Vector3(0,0,0));
     mCamera->setOrientation(Ogre::Quaternion(0.7071, 0.7071, 0, 0)); // rotate cameras by 90 degrees
-    // For hmd2
-    mCamera2->setPosition(Ogre::Vector3(0,0,0));
-    mCamera2->setOrientation(Ogre::Quaternion(0.7071, 0.7071, 0, 0)); // rotate cameras by 90 degrees
+    if (NumHMDs == 2)
+    {
+        // For hmd2
+        mCamera2->setPosition(Ogre::Vector3(0,0,0));
+        mCamera2->setOrientation(Ogre::Quaternion(0.7071, 0.7071, 0, 0)); // rotate cameras by 90 degrees
+    }
     std::cout << "Cameras Created" << std::endl;
 
     // Set the orientation of the cameras position
     stereo_cam_left->setPosition(openhmd->getLeftViewMatrix().getTrans());
     stereo_cam_right->setPosition(openhmd->getRightViewMatrix().getTrans());
-    // For hmd2 Set the orientation of the cameras position
-    stereo_cam_left2->setPosition(openhmd->getLeftViewMatrix().getTrans());
-    stereo_cam_right2->setPosition(openhmd->getRightViewMatrix().getTrans());
+    if (NumHMDs == 2)
+    {
+        // For hmd2 Set the orientation of the cameras position
+        stereo_cam_left2->setPosition(openhmd->getLeftViewMatrix().getTrans());
+        stereo_cam_right2->setPosition(openhmd->getRightViewMatrix().getTrans());
+    }
 
     // Set clip distances (something to do with focus I think)
     stereo_cam_left->setNearClipDistance(0.000012);
     stereo_cam_left->setFarClipDistance(200*120);
     stereo_cam_right->setNearClipDistance(0.000012);
     stereo_cam_right->setFarClipDistance(200*120);
-    // For hmd2
-    stereo_cam_left2->setNearClipDistance(0.000012);
-    stereo_cam_left2->setFarClipDistance(200*120);
-    stereo_cam_right2->setNearClipDistance(0.000012);
-    stereo_cam_right2->setFarClipDistance(200*120);
+    if (NumHMDs == 2)
+    {
+        // For hmd2
+        stereo_cam_left2->setNearClipDistance(0.000012);
+        stereo_cam_left2->setFarClipDistance(200*120);
+        stereo_cam_right2->setNearClipDistance(0.000012);
+        stereo_cam_right2->setFarClipDistance(200*120);
+    }
     std::cout << "Cameras positioned" << std::endl;
 
     // Put the cameras on the camera node
     stereo_cam_left->detachFromParent();
     stereo_cam_right->detachFromParent();
-    // For hmd2
-    stereo_cam_left2->detachFromParent();
-    stereo_cam_right2->detachFromParent();
+    if (NumHMDs == 2)
+    {
+        // For hmd2
+        stereo_cam_left2->detachFromParent();
+        stereo_cam_right2->detachFromParent();
+    }
 
     mCamera->attachObject(stereo_cam_left);
     mCamera->attachObject(stereo_cam_right);
-    // For hmd2
-    mCamera2->attachObject(stereo_cam_left2);
-    mCamera2->attachObject(stereo_cam_right2);
+    if (NumHMDs == 2)
+    {
+        // For hmd2
+        mCamera2->attachObject(stereo_cam_left2);
+        mCamera2->attachObject(stereo_cam_right2);
+    }
 
     // Setup viewports
     mWindow->removeAllViewports();
     leftVP = mWindow->addViewport(stereo_cam_left, 1, 0, 0, 0.5f, 1.0f);
     rightVP = mWindow->addViewport(stereo_cam_right, 2, 0.5f, 0, 0.5f, 1.0f);
-    // For hmd2
-    mWindow2->removeAllViewports();
-    leftVP2 = mWindow2->addViewport(stereo_cam_left2, 1, 0, 0, 0.5f, 1.0f);
-    rightVP2 = mWindow2->addViewport(stereo_cam_right2, 2, 0.5f, 0, 0.5f, 1.0f);
+    if (NumHMDs == 2)
+    {
+        // For hmd2
+        mWindow2->removeAllViewports();
+        leftVP2 = mWindow2->addViewport(stereo_cam_left2, 1, 0, 0, 0.5f, 1.0f);
+        rightVP2 = mWindow2->addViewport(stereo_cam_right2, 2, 0.5f, 0, 0.5f, 1.0f);
+    }
     std::cout << "Viewports added to window" << std::endl;
 
     // Assumes we are using the HTC Vive, else a different compositor should be added and used
@@ -202,15 +230,17 @@ branch on num hmds
         Ogre::CompositorInstance* leftComp = Ogre::CompositorManager::getSingletonPtr()->addCompositor(leftVP, "HMD/GenericAutoScaling");
         Ogre::CompositorInstance* rightComp = Ogre::CompositorManager::getSingletonPtr()->addCompositor(rightVP, "HMD/GenericAutoScaling");
         std::cout << "Enabling compositors" << std::endl;
-        if (!leftComp) std::cout << "why is it null" << std::endl;
         // Enable the compositors to show images
         leftComp->setEnabled(true);
         rightComp->setEnabled(true);
-        // For hmd2
-        Ogre::CompositorInstance* leftComp2 = Ogre::CompositorManager::getSingletonPtr()->addCompositor(leftVP2, "HMD/GenericAutoScaling");
-        Ogre::CompositorInstance* rightComp2 = Ogre::CompositorManager::getSingletonPtr()->addCompositor(rightVP2, "HMD/GenericAutoScaling");
-        leftComp2->setEnabled(true);
-        rightComp2->setEnabled(true);
+        if (NumHMDs == 2)
+        {
+            // For hmd2
+            Ogre::CompositorInstance* leftComp2 = Ogre::CompositorManager::getSingletonPtr()->addCompositor(leftVP2, "HMD/GenericAutoScaling");
+            Ogre::CompositorInstance* rightComp2 = Ogre::CompositorManager::getSingletonPtr()->addCompositor(rightVP2, "HMD/GenericAutoScaling");
+            leftComp2->setEnabled(true);
+            rightComp2->setEnabled(true);
+        }
     }
     else
     {
@@ -221,11 +251,14 @@ branch on num hmds
         Ogre::CompositorInstance* rightComp = Ogre::CompositorManager::getSingletonPtr()->addCompositor(rightVP, "HMD/GenericAutoScaling");
         leftComp->setEnabled(true);
         rightComp->setEnabled(true);
-        // For hmd2
-        Ogre::CompositorInstance* leftComp2 = Ogre::CompositorManager::getSingletonPtr()->addCompositor(leftVP2, "HMD/GenericAutoScaling");
-        Ogre::CompositorInstance* rightComp2 = Ogre::CompositorManager::getSingletonPtr()->addCompositor(rightVP2, "HMD/GenericAutoScaling");
-        leftComp2->setEnabled(true);
-        rightComp2->setEnabled(true);
+        if (NumHMDs == 2)
+        {
+            // For hmd2
+            Ogre::CompositorInstance* leftComp2 = Ogre::CompositorManager::getSingletonPtr()->addCompositor(leftVP2, "HMD/GenericAutoScaling");
+            Ogre::CompositorInstance* rightComp2 = Ogre::CompositorManager::getSingletonPtr()->addCompositor(rightVP2, "HMD/GenericAutoScaling");
+            leftComp2->setEnabled(true);
+            rightComp2->setEnabled(true);
+        }
     }
     std::cout << "HMD fully initialized" << std::endl;
 }
@@ -250,11 +283,14 @@ void OpenhmdDisplay::update(float wall_dt, float ros_dr)
     stereo_cam_left->setOrientation(oculusCameraOrientation);
     stereo_cam_right->setOrientation(oculusCameraOrientation);
 
-    // Get the orientation to update cameras
-    Ogre::Quaternion oculusCameraOrientation2 = openhmd->getQuaternion2();
-    // std::cout << oculusCameraOrientation << std::endl;
-    stereo_cam_left2->setOrientation(oculusCameraOrientation2);
-    stereo_cam_right2->setOrientation(oculusCameraOrientation2);
+    if (NumHMDs == 2)
+    {
+        // Get the orientation to update cameras
+        Ogre::Quaternion oculusCameraOrientation2 = openhmd->getQuaternion2();
+        // std::cout << oculusCameraOrientation << std::endl;
+        stereo_cam_left2->setOrientation(oculusCameraOrientation2);
+        stereo_cam_right2->setOrientation(oculusCameraOrientation2);
+    }
 }
 
 /************ Empty display reset ************/
